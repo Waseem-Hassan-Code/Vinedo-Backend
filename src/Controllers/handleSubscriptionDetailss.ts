1aa4470934f6011063f317bc48664f1da89d21a4
Umairmaqsood <umairmaqsood089@gmail.com>
2023-12-26 23:35:35 +0500

    bug fix

--- src/Controllers/handleSubscriptionDetailss.ts
+++ src/Controllers/handleSubscriptionDetailss.ts
@@ -18,7 +18,7 @@ import { authorizedUser } from "../Helpers/validateUser";
 export const setCreatorSub = async (
   req: express.Request,
   res: express.Response
-): Promise<void> => {
+) => {
   try {
     const { creatorId, subscriptionPrice, payPalEmail } = req.body;
 
@@ -27,7 +27,7 @@ export const setCreatorSub = async (
         message: 'Invalid request. "creatorId" is required.',
         result: {},
       };
-      res.status(400).json(response);
+      return res.status(400).json(response);
     }
 
     const authorized = await authorizedUser(creatorId);
@@ -36,7 +36,7 @@ export const setCreatorSub = async (
         message: "Invalid request. You are unauthorized for this request.",
         result: {},
       };
-      res.status(400).json(response);
+      return res.status(400).json(response);
     }
 
     const updatedData = await setCreatorSubscriptionDetails({
@@ -50,10 +50,10 @@ export const setCreatorSub = async (
         message: "Data update failed!",
         result: updatedData,
       };
-      res.status(400).json(response);
+      return res.status(400).json(response);
     }
 
-    res.status(200).json({
+    return res.status(200).json({
       message: "Data updated!",
       result: updatedData,
     });
@@ -62,7 +62,7 @@ export const setCreatorSub = async (
       message: "Internal server error!",
       result: { error },
     };
-    res.status(500).json(response);
+    return res.status(500).json(response);
   }
 };
 
