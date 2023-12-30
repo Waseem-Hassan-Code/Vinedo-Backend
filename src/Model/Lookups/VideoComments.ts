85e306a56f564fc7787cde69a8263a1879851f9d
Waseem Hassan <bman88873@gmail.com>
2023-12-31 00:18:13 +0500

    New vide get all comments API.

--- src/Model/Lookups/VideoComments.ts
+++ src/Model/Lookups/VideoComments.ts
@@ -1,3 +1,4 @@
+import { UserModel } from "../users";
 import { VideoCommentsModel } from "../videoComments";
 
 export const commentsAggregate = async (
@@ -8,40 +9,27 @@ export const commentsAggregate = async (
   try {
     const skip = (page - 1) * pageSize;
 
-    const aggregateQuery = [
-      {
-        $lookup: {
-          from: "user",
-          localField: "userId",
-          foreignField: "_id",
-          as: "userDetails",
-        },
-      },
-      {
-        $unwind: "$userDetails",
-      },
-      {
-        $project: {
-          _id: 0,
-          comment: 1,
-          videoId: videoId,
-          userId: 1,
-          userName: "$userDetails.name",
-        },
-      },
-      {
-        $skip: skip,
-      },
-      {
-        $limit: pageSize,
-      },
-    ];
+    const comments = await VideoCommentsModel.find({ videoId: videoId })
+      .skip(skip)
+      .limit(pageSize)
+      .exec();
 
-    const result = await VideoCommentsModel.aggregate(aggregateQuery).exec();
+    const commentsObject = await Promise.all(
+      comments.map(async (comment) => {
+        const user = await UserModel.findById(comment.userId);
 
-    return result;
+        return {
+          _id: comment._id,
+          comment: comment.comment,
+          userIdName: user ? user.name : "",
+          videoId: comment.videoId,
+        };
+      })
+    );
+
+    return commentsObject;
   } catch (error) {
-    console.error(`Error in commentsAggregate: ${error.message}`);
+    console.error(`Error in commentsAggregate for ImageId ${videoId}:`, error);
     throw error;
   }
 };
